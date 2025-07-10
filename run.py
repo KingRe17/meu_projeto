ccfb6307d5fc5953c6e62e1312f79553276c3c1f
KingRe17 <reynaldo17r@yahoo.com.br>
2025-07-10 14:57:48 -0300

    Atualiza run.py e adiciona suporte ao .env

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 from app import criar_app
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-app = criar_app()
+# Carrega variáveis do .env
+load_dotenv()
 
 if __name__ == '__main__': # se este arquivo for o principal que está rodando
-    app.run(debug=True) # inicia o servidor Flask com o modo debug ativado (ajuda a ver erros no terminal)
+    app = criar_app()
+    
+    HOST = os.getenv("HOST", "127.0.0.1")
+    PORT = int(os.getenv("PORT", 5000))
+    DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+    
+    app.run(host=HOST, port=PORT, debug=DEBUG) # inicia o servidor Flask com o modo debug ativado (ajuda a ver erros no terminal)
+
+
+'''
+os.getenv("variavel no .env", "se estiver vazio no .env usa esse")
+
+'''
